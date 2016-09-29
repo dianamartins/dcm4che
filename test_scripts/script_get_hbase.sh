46153c9cae4d4db06e228323f7deeabdb34926e9
Diana Martins <dscm.diana@gmail.com>
2016-09-29 10:13:37 +0100

    Improvements on get test scrip

--- test_scripts/script_get_hbase.sh
+++ test_scripts/script_get_hbase.sh
@@ -8,41 +8,43 @@ echo "Maximum $max"
 
 hosts=(cloud80 cloud81 cloud82 cloud83 cloud84)
 
-echo "Starting DCMQRSCP"
+#echo "Starting DCMQRSCP"
 
-ssh -i ~/.ssh/gsd_private_key gsd@cloud84 nohup /home/gsd/dcm4che/dcm4che-assembly/target/dcm4che-3.3.8-SNAPSHOT-bin/dcm4che-3.3.8-SNAPSHOT/bin/dcmqrscp -b DCMQRSCP:11113 --dicomdir /home/gsd/dcm4che/DICOMDIR -f /home/gsd/dcm4che/def-hbase-client.xml -d /home/gsd/dcm4che/images_database/ &
+#ssh -i ~/.ssh/gsd_private_key gsd@cloud84 nohup /home/gsd/dcm4che/dcm4che-assembly/target/dcm4che-3.3.8-SNAPSHOT-bin/dcm4che-3.3.8-SNAPSHOT/bin/dcmqrscp -b DCMQRSCP:11113 --dicomdir /home/gsd/dcm4che/DICOMDIR -f /home/gsd/dcm4che/def-hbase-client.xml -d /home/gsd/dcm4che/images_database/ &
 
-sleep 7
+#sleep 7
 
-echo "Removing previous dstats"
+#echo "Removing previous dstats"
 
-echo "cloud85"
+#echo "cloud85"
 
-rm /home/gsd/cloud85.csv
+#rm /home/gsd/cloud85.csv
 
-for host in "${hosts[@]}"
-do
-	echo $host
-	ssh -i ~/.ssh/gsd_private_key gsd@$host rm /home/gsd/$host.csv
-done
+#for host in "${hosts[@]}"
+#do
+#	echo $host
+#	ssh -i ~/.ssh/gsd_private_key gsd@$host rm /home/gsd/$host.csv
+#done
 
-echo "Removing previous results"
+#echo "Removing previous results"
 
-rm /home/gsd/dcm4che/results/resultsGETSCU.txt
+#rm /home/gsd/dcm4che/results/*
 
-echo "Creating new results file"
+#echo "Creating new results file"
 
-touch /home/gsd/dcm4che/results/resultsGETSCU.txt
+#touch /home/gsd/dcm4che/results/resultsGETSCU.txt
 
 echo "Starting dstat"
 
 echo "cloud85"
 
-for host in "${hosts[@]}" 
-do
-	echo $host
-	ssh -i ~/.ssh/gsd_private_key gsd@$host nohup dstat -t -c -d -m -n -r --output $host.csv --noheaders > /dev/null &
-done
+nohup dstat -t -c -d -m -n -r --output $host.csv --noheaders > /dev/null &
+
+#for host in "${hosts[@]}" 
+#do
+#	echo $host
+#	ssh -i ~/.ssh/gsd_private_key gsd@$host nohup dstat -t -c -d -m -n -r --output $host.csv --noheaders > /dev/null &
+#done
 
 echo "Starting get clients"
 
@@ -77,11 +79,11 @@ echo "cloud85"
 
 kill dstat
 
-for host in "${hosts[@]}" 
-do
-	echo $host
-	ssh -i ~/.ssh/gsd_private_key gsd@$host pkill dstat
-done
+#for host in "${hosts[@]}" 
+#do
+#	echo $host
+	#ssh -i ~/.ssh/gsd_private_key gsd@$host pkill dstat
+#done
 
 # echo "Copying result files to the localhost"
 
