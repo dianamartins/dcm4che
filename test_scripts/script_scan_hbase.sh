56cea39e3325e27aef94e1e195a676ccfad7dae1
Diana Martins <dscm.diana@gmail.com>
2016-10-04 15:41:08 +0100

    Corrected scan test scrip

--- test_scripts/script_scan_hbase.sh
+++ test_scripts/script_scan_hbase.sh
@@ -1,39 +1,72 @@
 #!/usr/bin/env bash
 
-runs=3000 # mudar
+#Teste para store
 
-max=$(ls -l /home/gsd/dcm4che/replicas/*.dcm | wc -l)
+#num_fixed_weight= $1 # alterar
 
-echo "Starting dstat cloud85"
+#num_images= $10 # alterar
 
-nohup dstat -t -c -d -m -n -r --output cloud85.csv --noheaders > /dev/null &
+hosts=(cloud80 cloud81 cloud82 cloud83 cloud84 cloud85)
 
-echo "Starting get clients"
+echo "Removing previous dstats"
 
-for i in $(seq $runs)
+for host in "${hosts[@]}"
 do
-	#r=$RANDOM
-	#f=$(( r %=$max-1 ))
-	#echo $f
-	echo $i
-
-	#if test $f -eq 0
-	#then
-	#	f=1
-	#fi
-	#echo "Image $f chosen"
-	weight=$(/home/gsd/dcm4che/dcm4che-assembly/target/dcm4che-3.3.8-SNAPSHOT-bin/dcm4che-3.3.8-SNAPSHOT/bin/dcmdump /home/gsd/dcm4che/replicas/$i.dcm | grep '(0010,1030)'| grep -o '\[[0-9.]*\]' | grep -o '[0-9.]*')
-
-	echo "Getting $weight"
-	/home/gsd/dcm4che/dcm4che-assembly/target/dcm4che-3.3.8-SNAPSHOT-bin/dcm4che-3.3.8-SNAPSHOT/bin/getscu -c DCMQRSCP@cloud84:11113 -L STUDY -m StudyInstanceUID=1 PatientWeight=$weight  #>> query.log
-	mv /home/gsd/dcm4che/results/resultsGETSCU.txt /home/gsd/dcm4che/results/scan$i.txt
-	#cat query.log	
+	echo $host
+	ssh -i ~/.ssh/gsd_private_key gsd@$host rm /home/gsd/$host.csv
 done
+
+echo "Removing previous results"
+
+ssh -i ~/.ssh/gsd_private_key gsd@cloud85 rm /home/gsd/dcm4che/results/resultsGETSCU.txt
+
+echo "Creating new results file"
+
+ssh -i ~/.ssh/gsd_private_key gsd@cloud85 touch /home/gsd/dcm4che/results/resultsGETSCU.txt
+
+echo "Starting DCMQRSCP"
+
+ssh -i ~/.ssh/gsd_private_key gsd@cloud84 nohup /home/gsd/dcm4che/dcm4che-assembly/target/dcm4che-3.3.8-SNAPSHOT-bin/dcm4che-3.3.8-SNAPSHOT/bin/dcmqrscp -b DCMQRSCP:11113 --dicomdir /home/gsd/dcm4che/DICOMDIR -f /home/gsd/dcm4che/def-hbase-client.xml -d /home/gsd/dcm4che/images_database/ &
+
+sleep 7
+
+echo "Starting dstat"
+
+for host in "${hosts[@]}" 
+do
+	echo $host
+	ssh -i ~/.ssh/gsd_private_key gsd@$host nohup dstat -t -c -d -m -n -r --output $host.csv --noheaders > /dev/null &
+done
+
+echo "Starting GetSCU"
+
+ssh -i ~/.ssh/gsd_private_key gsd@cloud85 /home/gsd/dcm4che/dcm4che-assembly/target/dcm4che-3.3.8-SNAPSHOT-bin/dcm4che-3.3.8-SNAPSHOT/bin/getscu -c DCMQRSCP@cloud84:11113 -L STUDY -m StudyInstanceUID=1 PatientWeight=70
+mv /home/gsd/dcm4che/results/resultsGETSCU.txt /home/gsd/dcm4che/results/scan$i.txt
 
 echo "Test ended"
 
+#echo "Stoping StoreSCP"
+
+# ssh -i ~/.ssh/gsd_private_key gsd@cloud84 ps aux | grep -i 1111[4] | awk {'print $2'} | kill
+
 echo "Stoping dstat"
 
-pkill dstat
+for host in "${hosts[@]}" 
+do
+	echo $host
+	ssh -i ~/.ssh/gsd_private_key gsd@$host pkill dstat
+done
+
+echo "Copying result files to the localhost"
+
+for host in "${hosts[@]}"
+do
+	echo $host
+	scp -i ~/.ssh/gsd_private_key gsd@$host:/home/gsd/$host.csv ~/tests_results/scan/
+done
+
+echo "Copying scan results"
+
+scp -i ~/.ssh/gsd_private_key gsd@cloud85:/home/gsd/dcm4che/results/* ~/tests_results/scan/
 
 echo "Done!"
